7de6c8ae3c3ef8fd050df766412dd0169544401e
FerbaDev <elferbarron1@gmail.com>
2024-04-22 00:58:20 -0300

    se trabaja en curstom router

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ patron telefono ------------------------ 02:56
 rutas con parametros del usuario ------- 03:05
 ruta con mail de parametro ------------- 03:12
 rutas que no coinciden con endpoint ---- 03:16
+validando parametros ------------------- 03:32
+middleware router.param ---------------- 03:37
+MUESTRA DE 3ERA PREENTREGA ------------- 03:41:43
+custom router -------------------------- 03:45
+user.router ---------------------------- 03:53
+
+
+
 
 
 
--- src/routes/clientes.router.js
+++ src/routes/clientes.router.js
@@ -23,6 +23,49 @@ router.get("/email/:email", (req, res) => {
     }
 })
 
+//Validando Parámetros: 
+//Supongamos que al crecer mi aplicación, voy a tener que generar muchas rutas que recibe el mismo parámetro: 
+
+router.get("/nombre/:cliente([a-z]+)", (req, res) => {
+    //Voy a obtener un recurso a partir del parametro cliente. 
+    res.send("Obteniendo un recurso para el cliente: " + req.params.cliente);
+})
+
+router.post("/nombre/:cliente([a-z]+)", (req, res) => {
+    //Voy a enviar un recurso a partir del parametro cliente. 
+    res.send("Enviando un recurso para el cliente: " + req.params.cliente);
+})
+
+router.put("/nombre/:cliente([a-z]+)", (req, res) => {
+    //Voy a actualizar un recurso a partir del parametro cliente. 
+    res.send("Actualizando un recurso para el cliente: " + req.params.cliente);
+})
+
+
+router.delete("/nombre/:cliente([a-z]+)", (req, res) => {
+    //Voy a eliminar un recurso a partir del parametro cliente. 
+    res.send("Eliminando un recurso para el cliente: " + req.params.cliente);
+})
+
+
+//Nos encontramos que en los 4 métodos hay lineas de código que son iguales y se van a repetir: 
+
+//a) Obtener el parámetro del cliente. 
+//b) Buscar el cliente en la base de datos. 
+//c) Una vez validado, continuar con la operacion que corresponda. 
+
+//Esto lo podemos simplificar creando un middleware llamado "router.param":
+
+router.param("cliente", (req, res, next, cliente) => {
+    const clientes = ["firulais", "lionel", "pepe"];
+
+    if(clientes.includes(cliente)) {
+        req.cliente = cliente; 
+        next(); 
+    } else {
+        res.status(404).send("Cliente no encontrado");
+    }
+})
 
 
 
